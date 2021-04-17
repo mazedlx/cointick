6fbf3decf4cdc79478eff65de2528f8cc5e63247
Christian Leo-Pernold <mazedlx@gmail.com>
2021-04-17 10:50:04 +0200

    WIP

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Spatie\ShortSchedule\ShortSchedule;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +25,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('doge:get')->everyMinute();
+    }
+
+    protected function shortSchedule(ShortSchedule $shortSchedule)
+    {
+        $shortSchedule->command('doge:get')->everySeconds(5);
     }
 
     /**
@@ -34,7 +39,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
